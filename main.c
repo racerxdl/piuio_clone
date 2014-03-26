4e54641c32a75634531fe500a10b6c40af925fa6
Lucas Teske <lucas@teske.com.br>
2014-03-26 19:22:18 -0300

    Added simple branch without muxers

--- main.c
+++ main.c
@@ -11,6 +11,9 @@
 /*     It uses V-USB for the USB Interface and few chips   */
 /***********************************************************/
 /*           This is main code from PIUIO Clone            */
+/*  This is the simple branch. This is for users that want */
+/*           just to connect to your DIY pads              */
+/*       The lights arent available on this one.           */
 /***********************************************************/
 /*                    License is GPLv3                     */
 /*  Please consult https://github.com/racerxdl/piuio_clone */
@@ -24,11 +27,43 @@
 #include <avr/pgmspace.h>   
 #include "usbdrv.h"
 #include "usbconfig.h"
+
 //    Some Macros to help
 
 #define GETBIT(port,bit) ((port) & (0x01 << (bit)))     //    Get Byte bit
 #define SETBIT(port,bit) ((port) |= (0x01 << (bit)))    //    Set Byte bit
 #define CLRBIT(port,bit) ((port) &= ~(0x01 << (bit)))   //    Clr Byte bit
+
+//    PIUIO Bytes
+// This is for Byte1 of the player short
+#define BTN_TEST      1
+#define BTN_SERVICE   6
+
+// This is for Byte0 of the player short
+#define SENSOR_LU     0
+#define SENSOR_RU     1
+#define SENSOR_CN     2
+#define SENSOR_LD     3
+#define SENSOR_RD     4
+
+//    Clone Port Map
+
+//  Player 1 Maps
+#define GET_LU_P1    GETBIT(PINB,0)         //  Player 1 Upper Left at PORTB0
+#define GET_RU_P1    GETBIT(PINB,1)         //  Player 1 Upper Right at PORTB1
+#define GET_CN_P1    GETBIT(PINB,2)         //  Player 1 Center at PORTB2
+#define GET_LD_P1    GETBIT(PINB,3)         //  Player 1 Down Left at PORTB3
+#define GET_RD_P1    GETBIT(PINB,4)         //  Player 1 Down Right at PORTB4
+#define GET_TEST     GETBIT(PINB,5)         //  Test Button at PORTB5
+
+//  Player 2 Maps
+#define GET_LU_P2    GETBIT(PINC,0)         //  Player 2 Upper Left at PORTC0
+#define GET_RU_P2    GETBIT(PINC,1)         //  Player 2 Upper Right at PORTC1
+#define GET_CN_P2    GETBIT(PINC,2)         //  Player 2 Center at PORTC2
+#define GET_LD_P2    GETBIT(PINC,3)         //  Player 2 Down Left at PORTC3
+#define GET_RD_P2    GETBIT(PINC,4)         //  Player 2 Down Right at PORTC4
+#define GET_SERVICE  GETBIT(PINC,5)         //  Service Button at PORTC5
+
 
 //    Some Vars to help
 static unsigned char LampData[8];       //    The LampData buffer received
@@ -36,11 +71,12 @@ static unsigned char InputData[8];      //    The InputData buffer to send
 static unsigned char datareceived = 0;  //    How many bytes we received
 static unsigned char dataLength = 0;    //    Total to receive
 
-static unsigned char Input[2];          //    The actual 16 bits Input data
+//static unsigned char Input[2];          //    The actual 16 bits Input data
 static unsigned char Output[2];         //    The actual 16 bits Output data
 
 USB_PUBLIC uchar usbFunctionWrite(uchar *data, uchar len) {
     //    This function will be only triggered when game writes to the lamps output.
+    //    I will keep this part of code, but we wont use to write to outputs. Because lack of IO
     unsigned char i;              
     for(i = 0; datareceived < 8 && i < len; i++, datareceived++)
                LampData[datareceived] = data[i];    
@@ -71,50 +107,24 @@ USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
 }
 
 void pollInputOutput()    {
-    //    This will set outputs and get inputs
-    //    The board will use 4067 muxer that is 16-to-1 muxer.
-    //    PORTC is the Muxer Selector.
-    //    PORTB0 is the Muxer Output
-    unsigned char inputn, tmp1,tmp2;    
-    SETBIT(PORTB,3);                                                        //    Disable the latches input
-    for(inputn=0;inputn<16;inputn++)    {
-        PORTC = inputn;                                                     //    Sets the muxer position
-        tmp1 = GETBIT(PINB,0);                                              //    Gets the input
-        if(tmp1 > 0)
-            SETBIT(Input[(inputn/16)*2],inputn%8);                          //    Sets if input = 1
-        else
-            CLRBIT(Input[(inputn/16)*2],inputn%8);                          //    Clears if input = 0
-    }
-    //    The board will use 4099 as output latch. Two are used to get 16 bit output.
-    //    PORTC is the address, PORTB1 is Data for First byte, PORTB2 is Data for Second
-    //    The Latch input enables are in PORTB3
-    //    So we will set two bits per loop cycle
-    CLRBIT(PORTB,3);                                                        //    Enable the latches input
-    for(inputn=0;inputn<8;inputn++)    {
-        PORTC = inputn;                                                     //    Sets the address
-        tmp1 = GETBIT(Output[0],inputn);                                    //    Gets the output data
-        tmp2 = GETBIT(Output[1],inputn);
-        if(tmp1 > 0) {SETBIT(PORTB,1);} else {CLRBIT(PORTB,1);};            //    Sets/Clear the output at latches
-        if(tmp2 > 0) {SETBIT(PORTB,2);} else {CLRBIT(PORTB,2);}; 
-        
-    }
-    SETBIT(PORTB,3);                                                        //    Disable the latches input, just in case.
-                                                                            //    Okay, so now we can set the output buffer, just in case the PC asks now the inputs
-
-    InputData[0] = ~Input[0];                                               //    Andamiro uses unsigned short here also
-    InputData[2] = ~Input[1];
+    //  This will get the inputs.
+    //  PIUIO actually sends inverted data.
+    InputData[0] = ~( (GET_LU_P1 << SENSOR_LU) & (GET_RU_P1 << SENSOR_RU) & (GET_CN_P1 << SENSOR_CN) &  (GET_LD_P1 << SENSOR_LD) & (GET_RD_P1 << SENSOR_RD) );
+    InputData[1] = ~( (GET_TEST  << BTN_TEST ) & (GET_SERVICE << BTN_SERVICE) );
+    InputData[2] = ~( (GET_LU_P2 << SENSOR_LU) & (GET_RU_P2 << SENSOR_RU) & (GET_CN_P2 << SENSOR_CN) &  (GET_LD_P2 << SENSOR_LD) & (GET_RD_P2 << SENSOR_RD) );
+    InputData[3] = 0xFF;
 }
 
 int main() {
     unsigned char i;
     wdt_enable(WDTO_1S);
 
-    DDRC = 255;
-    PORTC = 0;
-    DDRB = 0b00001110;
-    PORTB = 0;
+    DDRB = 0;
+    DDRC = 0;
+    
     for(i=0;i<8;i++)
         InputData[i] = 0xFF;
+        
     usbInit();
     usbDeviceDisconnect();                      // enforce re-enumeration
     for(i = 0; i<250; i++) {                    // wait 500 ms
